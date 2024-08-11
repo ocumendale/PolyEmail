9588ab1e71c131827774fd4c264ebf6e5cd1f393
KaeL <KaeL@WIN-AVB9VVIHKAN>
2024-08-12 02:05:25 +0800

    Add project files.

---
+++ PolyEmail/OcumenDaleBenedict.cs
@@ -0,0 +1,44 @@
+﻿class Notification
+{
+    public virtual void Notify()
+    {
+
+    }
+}
+class EmailNotification : Notification
+{
+    public override void Notify()
+    {
+        Console.WriteLine("You have Email Notification!");
+    }
+}
+class SmsNotification : Notification
+{
+    public override void Notify()
+    {
+        Console.WriteLine("You have SMS Notification!");
+    }
+}
+class PushNotification : Notification
+{
+    public override void Notify()
+    {
+        Console.WriteLine("You have Push Notification!");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        EmailNotification emailNotification = new EmailNotification();
+        SmsNotification smsNotification = new SmsNotification();
+        PushNotification pushNotification = new PushNotification();
+
+        Notification[] notifications = { emailNotification, smsNotification, pushNotification };
+
+        foreach (Notification n in notifications)
+        {
+            n.Notify();
+        }
+    }
+}
